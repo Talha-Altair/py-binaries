bc23584c95d5d511417f53bfacfe20a4988c8744
Talha-Altair <cooltalha27@gmail.com>
2021-12-28 18:09:48 +0530

    Updates for 2021-12-28 18:09:48

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 #!python3
 
 from flask import Flask, render_template, request, redirect, url_for, flash
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 
-    return render_template('index.html')
+    data = pd.read_csv('data.csv').to_json()
+
+    return render_template('index.html', data = data)
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,3 @@
+```
+pyinstaller --specpath /opt/bk/spec --distpath /opt/bk/dist --workpath /opt/bk/build testscript.py
+```
